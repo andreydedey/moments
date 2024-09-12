2d336c07858ad9aa8ae86bda2149bef1bd87023a
Andrey Oliveira <dedey.cardoso@gmail.com>
2024-09-12 16:02:23 -0300

    some corrections, and file logic complet

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -32,9 +32,15 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('description')!;
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0]
+
+    this.momentForm.patchValue({image: file})
+  }
+
   submit() {
     if (this.momentForm.invalid)
 
-    console.log("enviou formulario");
+    console.log(this.momentForm.value);
   }
 }
